bc1bd3ae4bc1e903122afd92ba0db837b8b72110
Bastien Fontaine <b.fontaine@thecodingmachine.com>
2023-12-04 17:38:56 +0100

    feat(app): add zoom in animation on home screen

--- src/components/templates/HomePageTemplate/HomePageTemplate.tsx
+++ src/components/templates/HomePageTemplate/HomePageTemplate.tsx
@@ -1,16 +1,70 @@
 import AppTitle from '../../atoms/AppTitle/AppTitle';
 import BasicButton from '../../atoms/BasicButton/BasicButton';
-import React from 'react';
-import {View} from 'react-native';
+import React, {useRef} from 'react';
+import {Animated, StyleSheet, View} from 'react-native';
 
 type Props = React.ComponentProps<typeof BasicButton> &
   React.ComponentProps<typeof AppTitle>;
 
 export default function HomePageTemplate(props: Props) {
+  const zoomAnim = useRef(new Animated.Value(1)).current;
+  const translateAnim = useRef(new Animated.Value(0)).current;
+
+  const zoomIn = () => {
+    Animated.timing(zoomAnim, {
+      toValue: 50,
+      duration: 500,
+      useNativeDriver: true,
+    }).start(({finished}) => {
+      if (finished) {
+        props.onPress();
+      }
+    });
+  };
+
+  const translate = () => {
+    Animated.timing(translateAnim, {
+      toValue: 10,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handlePress = () => {
+    zoomIn();
+    translate();
+  };
+
   return (
     <View>
       <AppTitle AppTitle={props.AppTitle} />
-      <BasicButton onPress={props.onPress} buttonTitle={props.buttonTitle} />
+      <Animated.View
+        style={[
+          styles.buttonContainer,
+          {
+            transform: [
+              {
+                scale: zoomAnim,
+              },
+              {
+                translateY: translateAnim,
+              },
+            ],
+          },
+        ]}>
+        <BasicButton
+          onPress={handlePress}
+          buttonTitle={props.buttonTitle}
+          style={styles.beginButton}
+        />
+      </Animated.View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  beginButton: {
+    backgroundColor: '#E8EAED',
+  },
+  buttonContainer: {},
+});
